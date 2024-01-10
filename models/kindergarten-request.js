a27d3e2e06723dd163e6cf5efb379834d6f1d8bd
Husain Salem <husalem.7@gmail.com>
2024-01-10 23:05:33 +0300

    working on kindergarten-request netPrice

--- models/kindergarten-request.js
+++ models/kindergarten-request.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const Comstum = require('./costum');
+const Costum = require('./costum');
 const Addition = require('./service-adds');
 
 const Schema = mongoose.Schema;
@@ -17,14 +17,20 @@ const serviceSchema = new Schema(
     },
     costums: [
       {
-        title: String,
-        imagePath: String,
-        sizeName: String,
-        sizePrice: {
-          type: Number,
+        costum: {
+          type: Schema.Types.ObjectId,
+          ref: 'Costum',
           required: true
         },
-        wooden: Boolean
+        size: {
+          type: Schema.Types.ObjectId,
+          ref: 'PaperSize',
+          required: true
+        },
+        additions: [{
+          type: Schema.Types.ObjectId,
+          ref: 'ServiceAdds'
+        }]
       }
     ],
     friendName: String,
@@ -46,22 +52,42 @@ const serviceSchema = new Schema(
 );
 
 requestSchema.pre(
-  'save',
+  ['save', 'updateOne'],
   async function (next) {
     let additions = [];
 
-    if (this.additions?.length) {
-      const additionsIds = this.additions.map((objectId) => objectId._id);
-      additions = await Addition.find({ _id: { $in: additionsIds } });
+    let request = this.op === 'updateOne' ? this._update : this;
+
+    let sizesIds = [];
+    let costAddsIdsSet = new Set();
+    const costumsIds = request.costums.map((item) => {
+      sizesIds.push(item.size);
+      
+      if (item.additions && Array.isArray(item.additions)) {
+        for (let i = 0; i < item.additions.length; i++) {
+          costAddsIdsSet.add(item.additions[i]);
+        }
+      }
+
+      return item.costum;
+    });
+
+    const costAddsIds = Array.from(costAddsIdsSet);
+
+    const costums = await Costum.find({ _id: { $in: costumsIds } }).populate('paperSize');
+
+    if (request.additions?.length) {
+      // const additionsIds = this.additions.map((objectId) => objectId._id);
+      additions = await Addition.find({ _id: { $in: request.additions } });
     }
 
     // Calculate costums prices
-    const costumsPrice = this.costums.reduce((total, costum) => costum.sizePrice, 0);
+    const costumsPrice = request.costums.reduce((total, costum) => costum.sizePrice, 0);
 
     // Calculate additional services prices
     const addsPrice = additions.reduce((total, service) => total + service.netPrice, 0);
 
-    this.netPrice = costumsPrice + addsPrice;
+    request.netPrice = costumsPrice + addsPrice;
 
     next();
   }
