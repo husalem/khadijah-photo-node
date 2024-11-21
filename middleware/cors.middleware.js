cc1b44a2a4cbd5f9fffc13c92439326d4afe7a1c
Husain Salem <husalem.7@gmail.com>
2024-11-21 18:46:41 +0300

    Remove opener policy

--- middleware/cors.middleware.js
+++ middleware/cors.middleware.js
@@ -2,15 +2,16 @@ const origin = '*';
 const methods = 'GET, POST, PUT, PATCH, DELETE, OPTIONS';
 const headers = 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization';
 const embedder = 'require-corp';
-const opener = 'same-site';
+const sameSite = 'same-site';
+const sameOrigin = 'same-origin';
 
 module.exports = (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', origin);
   res.setHeader('Access-Control-Allow-Methods', methods);
   res.setHeader('Access-Control-Allow-Headers', headers);
   res.setHeader('Cross-Origin-Embedder-Policy', embedder);
-  // res.setHeader('Cross-Origin-Opener-Policy', opener);
-  // res.setHeader('Cross-Origin-Resource-Policy', opener);
+  res.setHeader('Cross-Origin-Opener-Policy', sameOrigin);
+  res.setHeader('Cross-Origin-Resource-Policy', sameOrigin);
 
   if (req.method === 'OPTIONS') {
     res.sendStatus(200);
